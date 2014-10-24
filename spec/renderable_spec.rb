3a2c66ecfdc904b087e357060785574c062d5657
Tommaso Barbugli <tbarbugli@gmail.com>
2014-10-24 16:47:12 +0200

    fix render spec

--- spec/renderable_spec.rb
+++ spec/renderable_spec.rb
@@ -10,19 +10,19 @@ describe 'StreamRails::Renderable' do
 
     it "picks the template based on the verb" do
       activity = StreamRails::ActivityResult.new().from_activity({"verb"=> "like"})
-      @actionview.should_receive(:render).with({:partial=>"activity/like", :locals=>{:activity=>activity, :parameters=>{}}})
+      @actionview.should_receive(:render).with({:partial=>"activity/like", :layout=>nil, :locals=>{:activity=>activity, :parameters=>{}}})
       StreamRails::Renderable.render(activity, @actionview)
     end
 
     it "should be able to change partial_root" do
       activity = StreamRails::ActivityResult.new().from_activity({"verb"=> "like"})
-      @actionview.should_receive(:render).with({:partial=>"custom/like", :partial_root=>"custom", :locals=>{:activity=>activity, :parameters=>{:partial_root=>"custom"}}})
+      @actionview.should_receive(:render).with({:partial=>"custom/like", :partial_root=>"custom", :layout=>nil, :locals=>{:activity=>activity, :parameters=>{:partial_root=>"custom"}}})
       StreamRails::Renderable.render(activity, @actionview, {:partial_root=>'custom'})
     end
 
     it "should be able to send extra context" do
       activity = StreamRails::ActivityResult.new().from_activity({"verb"=> "like"})
-      @actionview.should_receive(:render).with({:partial=>"activity/like", :tommaso=>1, :locals=>{:activity=>activity, :parameters=>{:tommaso=>1}}})
+      @actionview.should_receive(:render).with({:partial=>"activity/like", :tommaso=>1, :layout=>nil, :locals=>{:activity=>activity, :parameters=>{:tommaso=>1}}})
       StreamRails::Renderable.render(activity, @actionview, {:tommaso=>1})
     end
   end
