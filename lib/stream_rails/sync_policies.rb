f115b3f2576b54af03bd4b6cf46d876f8fc65b97
Rui Onodera <deraru@gmail.com>
2015-02-05 11:41:06 +0900

    Add active record 3 support

--- lib/stream_rails/sync_policies.rb
+++ lib/stream_rails/sync_policies.rb
@@ -5,7 +5,7 @@ module StreamRails
     module SyncCreate
 
       def self.included(base)
-        base.after_commit :add_to_feed, on: [:create]
+        base.after_commit :add_to_feed, on: :create
       end
 
       private
@@ -21,7 +21,7 @@ module StreamRails
     module SyncDestroy
 
       def self.included(base)
-        base.after_commit :remove_from_feed, on: [:destroy]
+        base.after_commit :remove_from_feed, on: :destroy
       end
 
       private
