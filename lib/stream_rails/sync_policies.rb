65d8e871e2cc8b5b2d84794fb8f88adecb72a5c4
Kenny Meyer <knny.myer@gmail.com>
2015-12-04 14:19:10 -0300

    Update implementation to make it work

--- lib/stream_rails/sync_policies.rb
+++ lib/stream_rails/sync_policies.rb
@@ -8,15 +8,11 @@ module StreamRails
         if base.respond_to? :after_commit
           base.after_commit :add_to_feed, on: :create
         else
-          base.instance_eval do
-            define_method(:add_to_feed) {
-              begin
-                StreamRails.feed_manager.created_activity(self)
-              rescue Exception => e
-                StreamRails.logger.error "Something went wrong creating an activity: #{e}"
-                raise
-              end
-            }
+          base.class_eval do
+            define_method(:_after_create) do |*args|
+              super(*args)
+              add_to_feed
+            end
           end
         end
       end
@@ -39,14 +35,10 @@ module StreamRails
           base.after_commit :remove_from_feed, on: :destroy
         else
           base.instance_eval do
-            define_method(:remove_from_feed) {
-              begin
-                StreamRails.feed_manager.destroyed_activity(self)
-              rescue Exception => e
-                StreamRails.logger.error "Something went wrong deleting an activity: #{e}"
-                raise
-              end
-            }
+            define_method(:_before_destroy) do |*args|
+              super(*args)
+              remove_from_feed
+            end
           end
         end
       end
