bd1dc3f5f5f9a95da7b99a00056defbfa2fcc04a
Tommaso Barbugli <tbarbugli@gmail.com>
2018-02-25 21:41:55 +0100

    update specs

--- spec/feed_manager_spec.rb
+++ spec/feed_manager_spec.rb
@@ -27,37 +27,15 @@ describe 'StreamRails::FeedManager' do
     specify { feed_manager.get_user_feed(1).should be_an_instance_of Stream::Feed }
     specify { feed_manager.get_user_feed(1).id.should eq 'user:1' }
     specify { feed_manager.get_news_feeds(1).should be_an_instance_of Hash }
-    specify { feed_manager.get_news_feeds(1)[:flat].should be_an_instance_of Stream::Feed }
-    specify { feed_manager.get_news_feeds(1)[:flat].id.should eq 'flat:1' }
-    specify { feed_manager.get_news_feeds(1)[:aggregated].should be_an_instance_of Stream::Feed }
-    specify { feed_manager.get_news_feeds(1)[:aggregated].id.should eq 'aggregated:1' }
+    specify { feed_manager.get_news_feeds(1)[:timeline].should be_an_instance_of Stream::Feed }
+    specify { feed_manager.get_news_feeds(1)[:timeline].id.should eq 'flat:1' }
+    specify { feed_manager.get_news_feeds(1)[:timeline_aggregated].should be_an_instance_of Stream::Feed }
+    specify { feed_manager.get_news_feeds(1)[:timeline_aggregated].id.should eq 'aggregated:1' }
     specify { feed_manager.get_notification_feed(1).should be_an_instance_of Stream::Feed }
     specify { feed_manager.get_feed('flat', 1).should be_an_instance_of Stream::Feed }
   end
 
   context 'follow and unfollow' do
-    context 'StreamRails enabled' do
-      let(:feed_manager) { StreamRails.feed_manager }
-
-      specify do
-        feed_manager.follow_user(1, 2)
-        user2 = feed_manager.get_user_feed(2)
-        u2followers = user2.followers
-        u2followers['results'][0]['feed_id'].should eq 'aggregated:1'
-        u2followers['results'][1]['feed_id'].should eq 'flat:1'
-      end
-      specify do
-        user2 = feed_manager.get_user_feed(2)
-        followers1 = user2.followers
-        followers1['results'].should_not eq []
-
-        feed_manager.unfollow_user(1, 2)
-        followers2 = user2.followers
-
-        followers2.should_not eq followers1
-        followers2['results'].should eq []
-      end
-    end
 
     context 'StreamRails disabled' do
       let(:feed_manager) { StreamRails.feed_manager }
